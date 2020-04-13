d7fc4b3a1e7064864f7ed804151d4a345f1c24c2
Sven Mijic <svenmijic@gmail.com>
2020-04-13 14:56:03 +0200

    Edit Project.cs

--- Domain/Project.cs
+++ Domain/Project.cs
@@ -5,7 +5,7 @@ namespace Domain
 {
     public class Project
     {
-        public long Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
